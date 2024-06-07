99515e220fa463ff33010909ea584ab05b0e9056
bangnokia <bangnokia@gmail.com>
2024-06-07 19:08:50 +0700

    wip

--- scripts/tune-fpm.sh
+++ scripts/tune-fpm.sh
@@ -5,6 +5,9 @@ CONFIG_DIR="/opt/php-7.2.18/etc/php-fpm.d"
 NEW_MAX_CHILDREN="5"
 NEW_MAX_REQUESTS="200"
 
+# Backup the configuration files
+cp -r "$CONFIG_DIR" "$CONFIG_DIR.bak.$(date +%Y%m%d%H%M)"
+
 # Check if the directory exists
 if [[ -d "$CONFIG_DIR" ]]; then
     # Loop through all files in the directory
@@ -12,7 +15,6 @@ if [[ -d "$CONFIG_DIR" ]]; then
         # Check if the file exists (in case no .conf files are found)
         if [[ -f "$config_file" ]]; then
             echo "Processing config file: $config_file"
-            # Add your processing logic here
 	        # Update pm.max_children and pm.max_requests values
             sed -i.bak -E "s/^(pm\.max_children\s*=\s*).*/\1$NEW_MAX_CHILDREN/" "$config_file"
             sed -i.bak -E "s/^(pm\.max_requests\s*=\s*).*/\1$NEW_MAX_REQUESTS/" "$config_file"
