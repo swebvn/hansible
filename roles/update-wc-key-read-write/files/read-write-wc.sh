577c7fa2441db7e5104086dae7552df94d4617cb
bangnokia <bangnokia@gmail.com>
2024-07-08 17:00:34 +0700

    update db permissions

--- roles/update-wc-key-read-write/files/read-write-wc.sh
+++ roles/update-wc-key-read-write/files/read-write-wc.sh
@@ -1,1 +1,22 @@
-echo "script wc has been executed"
+# loop thru all folder /home/username/domains/domain.xxx/public_html
+
+for d in /home/*/domains/*/public_html; do
+    # check if wp-config.php exists
+    if [ ! -f "$d/wp-config.php" ]; then
+        continue
+    fi
+
+    # get the database name
+    dbname=$(grep DB_NAME "$d/wp-config.php" | cut -d \' -f 4)
+
+
+    # update permissions = read_write
+    mysql -u root -e "UPDATE $dbname.wp_woocommerce_api_keys SET permissions = 'read_write' WHERE description like '%portal%';"
+
+    # if the query above error, echo error
+    if [ $? -ne 0 ]; then
+        echo "Error: $dbname"
+    else
+        echo "Updated permissions: $dbname"
+    fi
+done
