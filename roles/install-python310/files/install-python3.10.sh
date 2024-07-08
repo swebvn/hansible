98ddfa2da9496c9924eb9e4b3d89a5a1cbfae2bd
bangnokia <bangnokia@gmail.com>
2024-07-08 12:09:54 +0700

    refactor to task

--- roles/install-python310/files/install-python3.10.sh
+++ roles/install-python310/files/install-python3.10.sh
@@ -3,7 +3,7 @@ sudo yum groupinstall -y "Development Tools"
 sudo yum install -y openssl-devel bzip2-devel libffi-devel zlib-devel
 
 # Download and extract Python 3.10 source code
-cd /tmp
+cd /home/deploy
 sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 sudo tar xzf Python-3.10.0.tgz
 cd Python-3.10.0
