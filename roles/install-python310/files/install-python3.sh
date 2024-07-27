1b83906bfb2dd52fd6f6483a95b0f64eb3382e52
bangnokia <bangnokia@gmail.com>
2024-07-27 17:14:12 +0700

    wip

--- roles/install-python310/files/install-python3.sh
+++ roles/install-python310/files/install-python3.sh
@@ -1,7 +1,7 @@
 # Install Development Tools and Libraries
 sudo yum update -y
 sudo yum groupinstall -y "Development Tools"
-sudo yum install -y openssl-devel bzip2-devel libffi-devel zlib-devel
+sudo yum install -y openssl-devel bzip2-devel libffi-devel zlib-devel gcc
 
 # Download and extract Python 3.10 source code
 cd /home/deploy
@@ -9,11 +9,6 @@ sudo wget https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz
 sudo tar xzf Python-3.10.0.tgz
 cd Python-3.10.0
 
-# check if gcc installed, if not install it
-gcc --version
-if [ $? -ne 0 ]; then
-  sudo yum install -y gcc
-fi
 
 # Build and install Python 3.10
 sudo ./configure --enable-optimizations
