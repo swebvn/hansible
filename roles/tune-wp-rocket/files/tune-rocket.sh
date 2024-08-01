f0b30424208f44f5e478e07e95aabe0ff14d030c
bangnokia <bangnokia@gmail.com>
2024-08-01 15:45:27 +0700

    wip

--- roles/tune-wp-rocket/files/tune-rocket.sh
+++ roles/tune-wp-rocket/files/tune-rocket.sh
@@ -1,10 +1,13 @@
 for plugin_dir in /home/*/domains/*/public_html/wp-content/plugins/; do
   # check of wp-rocket folder exists
   if [ -d "$plugin_dir/wp-rocket" ]; then
+    #change current directory to the plugin directory
+    cd $plugin_dir
+
     echo "Tuning WP Rocket settings for $plugin_dir"
 
     # we need check if the
     wp-cli option patch update wp_rocket_settings manual_preload 0 --allow-root
-    wp-cli option patch update wp_rocket_settings purge_cron_interval 300 --allow-root
+    wp-cli option patch update wp_rocket_settings purge_cron_interval 322 --allow-root
   fi
 done
