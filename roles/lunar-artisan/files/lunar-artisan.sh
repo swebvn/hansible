2ab2c6f655eddf4da6fc858ba5da0a3f905fef67
bangnokia <bangnokia@gmail.com>
2024-09-16 15:00:44 +0700

    run the artisan command on server

--- roles/lunar-artisan/files/lunar-artisan.sh
+++ roles/lunar-artisan/files/lunar-artisan.sh
@@ -1,8 +1,18 @@
 # loop thu all the directory in format
 
+command=$1
+
 for dir in /home/*/domains/*/public_html; do
     user=$(echo $dir | cut -d'/' -f3)
     domain=$(echo $dir | cut -d'/' -f5)
 
-    echo "Current user: $user, domain: $domain"
+    if [ ! -f "$dir/.env" ]; then
+        continue
+    fi
+
+    # run the artisan command
+    su - $user -c "{
+        cd $dir
+        $command
+    }"
 done
